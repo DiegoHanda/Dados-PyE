72e79f28bbad82a96ae0a3217b4d71e351500d31
tomasdarracq <juan.darracq@correo.ucu.edu.uy>
2024-05-04 18:00:51 -0300

    .

--- Dados.py
+++ Dados.py
@@ -5,15 +5,31 @@ def tirar_dado():
     return dado
 
 def juego():
-    puntosJuan = estrategia_juan()
-    puntosMaria = estrategia_maria(puntosJuan)
-    print("      RESULTADO    \n----------------------\n| Nombre |  Puntos   |\n----------------------\n| Juan   |    ", puntosJuan, "    |\n| María  |    ", puntosMaria, "    |\n----------------------")
-    if puntosJuan > puntosMaria:
-        print("GANADOR: JUAN")
-    elif puntosMaria > puntosJuan:
-        print("GANADOR: MARÍA")
-    else:
-        print("EMPATE")
+    cantidadJuegosString = input("Ingrese cuantas veces quiere simular el juego, 1000, 10000 o 100000: ")
+    cantidadJuegos = int(cantidadJuegosString)
+    cantVictoriasJuan = 0
+    cantVictoriasMaria = 0
+    cantEmpates = 0
+    while(cantidadJuegos>0):
+        puntosJuan = estrategia_juan()
+        puntosMaria = estrategia_maria(puntosJuan)
+        print("      RESULTADO    \n----------------------\n| Nombre |  Puntos   |\n----------------------\n| Juan   |    ", puntosJuan, "    |\n| Maria  |    ", puntosMaria, "    |\n----------------------")
+        if puntosJuan > puntosMaria:
+            print("GANADOR: JUAN")
+            cantVictoriasJuan+=1
+        elif puntosMaria > puntosJuan:
+            print("GANADOR: MARiA")
+            cantVictoriasMaria+=1
+        else:
+            print("EMPATE")
+            cantEmpates+=1
+        cantidadJuegos = cantidadJuegos - 1
+        
+    print("Cantidad de victorias de Juan", cantVictoriasJuan)
+    print("Cantidad de victorias de Maria", cantVictoriasMaria)
+    print("Empates", cantEmpates)
+    
+             
 
 def estrategia_juan():
     puntosJuan = 0
@@ -22,7 +38,7 @@ def estrategia_juan():
     print("\nPRIMER LANZAMIENTO JUAN\n-----------------------\n|  DADO 1  |  DADO 2  |\n-----------------------\n|    ", dado1, "    |   ", dado2, "   |\n-----------------------")
     if dado1 == 4 or dado2 == 4:
         puntosJuan = dado1 + dado2 - 4
-        print("Puntuación: ", puntosJuan, "\n")
+        print("Puntuacion: ", puntosJuan, "\n")
         if dado1 < 4:
             print("Lanza de nuevo el dado 1...")
             dado1 = tirar_dado()
@@ -32,8 +48,8 @@ def estrategia_juan():
         if dado1 == 4 or dado2 == 4:
             puntosJuan = dado1 + dado2 - 4
     else:
-        print("Puntuación", puntosJuan, "\n")
-        print("Lanza de nuevo ambos dados...")
+        print("Puntuacion", puntosJuan, "\n")
+        print("Juan lanza de nuevo ambos dados...")
         dado1 = tirar_dado()
         dado2 = tirar_dado()
         if dado1 == 4 or dado2 == 4:
@@ -41,19 +57,19 @@ def estrategia_juan():
         else: 
             puntosJuan = 0
     print("\nSEGUNDO LANZAMIENTO JUAN\n-----------------------\n|  DADO 1  |  DADO 2  |\n-----------------------\n|    ", dado1, "    |   ", dado2, "   |\n-----------------------")
-    print("Puntuación Final de Juan: ", puntosJuan, "\n")
+    print("Puntuacion Final de Juan: ", puntosJuan, "\n")
     return puntosJuan
         
 def estrategia_maria(puntosJuan):
     puntosMaria = 0
     dado1 = tirar_dado()
     dado2 = tirar_dado()
-    print("\nPRIMER LANZAMIENTO MARÍA\n-----------------------\n|  DADO 1  |  DADO 2  |\n-----------------------\n|    ", dado1, "    |   ", dado2, "   |\n-----------------------")
+    print("\nPRIMER LANZAMIENTO MARiA\n-----------------------\n|  DADO 1  |  DADO 2  |\n-----------------------\n|    ", dado1, "    |   ", dado2, "   |\n-----------------------")
     if dado1 == 4 or dado2 == 4:
         puntosMaria = dado1 + dado2 - 4
-        print("Puntuación:", puntosMaria, "\n")
+        print("Puntuacion:", puntosMaria, "\n")
         if puntosMaria > puntosJuan:
-            print ("María se retira\n")
+            print ("Maria se retira\n")
             return puntosMaria
         else:
             if dado1 == 4 & dado2 == 4:
@@ -68,16 +84,17 @@ def estrategia_maria(puntosJuan):
             if dado1 == 4 or dado2 == 4:
                 puntosMaria = dado1 + dado2 - 4
     else:
-        print("Puntuación: ", puntosMaria, "\n")
-        print("Lanza de nuevo ambos dados...")
+        print("Puntuacion: ", puntosMaria, "\n")
+        print("Maria lanza de nuevo ambos dados...")
         dado1 = tirar_dado()
         dado2 = tirar_dado()
         if dado1 == 4 or dado2 == 4:
             puntosMaria = dado1 + dado2 - 4
         else: 
             puntosMaria = 0
-    print("\nSEGUNDO LANZAMIENTO MARÍA\n-----------------------\n|  DADO 1  |  DADO 2  |\n-----------------------\n|    ", dado1, "    |   ", dado2, "   |\n-----------------------")
-    print("Puntuación Final de María: ", puntosMaria, "\n")
+    print("\nSEGUNDO LANZAMIENTO MARiA\n-----------------------\n|  DADO 1  |  DADO 2  |\n-----------------------\n|    ", dado1, "    |   ", dado2, "   |\n-----------------------")
+    print("Puntuacion Final de Maria: ", puntosMaria, "\n")
     return puntosMaria
+
 
 juego()
